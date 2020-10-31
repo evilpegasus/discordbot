49aa5340f3f219e3a5ffc4e90eeabad836d3dade
Ming Fong <mingfong07@hotmail.com>
2020-10-30 23:06:01 -0700

    setup documentation

--- deploy.py
+++ deploy.py
@@ -1,7 +1,12 @@
 from __future__ import print_function,unicode_literals
 import subprocess
 from time import sleep
-
+"""
+HOW TO DEPLOY THE BOT (LINUX)
+1. Run this file to copy new mingbot.py to host machine
+2. SSH into host machine and cd to ~/discordbot
+3. Run start.bash on host machine to kill old bot process and start a new one
+"""
 # subprocess.run(r"scp -i ocf_key .\mingbot.py .\secrets.py .\start.bash mingfong@ssh.ocf.berkeley.edu:~/discordbot")
 subprocess.run(r"scp -i ocf_key .\mingbot.py mingfong@ssh.ocf.berkeley.edu:~/discordbot")
 
